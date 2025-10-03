db56ef5e1264f42347e0dd306a76caf9905f8ee4
Luis Palacio <palaciodimasluisenrique@gmail.com>
2025-10-03 00:53:30 -0500

    setting route componen

--- src/components/steps/CardSteps.jsx
+++ src/components/steps/CardSteps.jsx
@@ -3,7 +3,7 @@ import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
 import Step4 from "./Step4";
-import ThanksStep from "./thanksStep";
+import ThanksStep from "./ThanksStep";
 
 function CardSteps() {
     const { step } = useStep();
